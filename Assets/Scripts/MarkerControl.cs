29a298b38200bad736ec9245c9e9e04f07a9a76d
Jeffrey Scarmazzi <jeff8977@esri.com>
2022-01-25 23:30:40 -0500

    Still Building Rough Outlines

---
+++ Assets/Scripts/MarkerControl.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerControl : MonoBehaviour
+{
+    void OnMouseOver()
+    {
+        Debug.Log("Game Object Selected");
+    }
+}
